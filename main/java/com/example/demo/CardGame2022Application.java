fd4d30b11e474d28cbf035ea3c4eddff032bfd0a
joipoi <joel.ekne@skymill.io>
2022-05-09 20:29:30 +0200

    first commit

---
+++ main/java/com/example/demo/CardGame2022Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGame2022Application {
+
+	public static void main(String[] args) {
+		System.out.println("he");
+		SpringApplication.run(CardGame2022Application.class, args);
+	}
+
+}
